f1f04e85cd4a53b42a7c276f88fcd2e610734105
Somil <somilagrawal2004@gmail.com>
2025-12-18 21:57:18 +0530

    commit

--- server/Routes/Car.js
+++ server/Routes/Car.js
@@ -8,8 +8,15 @@ const {
   deleteCar,
 } = require("../Controller/Car");
 const { checkforauth, AdminAuth } = require("../Middleware/Auth");
+const upload = require("../Middleware/Multer");
 
-router.post("/add-car", checkforauth, AdminAuth, addCar);
+router.post(
+  "/add-car",
+  checkforauth,
+  AdminAuth,
+  upload.single("image"),
+  addCar
+);
 router.get("/all-cars", AdminAuth, getAllCars);
 router.get("/:id", AdminAuth, getCarById);
 router.patch("/update-car/:id", checkforauth, AdminAuth, updateCar);
